cdf7c8c85683051f7d42734bb659fb33cd9e9542
richifytr-maker <richify.tr@gmail.com>
2025-11-29 18:16:08 +0300

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tjgaskhafefygiztmffg.supabase.co' // örnek: https://xyzcompany.supabase.co
-const supabaseAnonKey = 'SUPABASE_ANON_KEY_BURAYA'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZ2Fza2hhZmVmeWdpenRtZmZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MjY1NDgsImV4cCI6MjA4MDAwMjU0OH0.xSkCO5oF_MoHItB1LkCOGuuOBO-FVGVJbj1Mlk93n_g'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
